ecf333b3f498d28ae92df4ac9b2b0bbf8974c138
sibistean95 <adriansibistean9501@gmail.com>
2024-05-11 17:19:59 +0300

    update in script

--- verify_for_malicious.sh
+++ verify_for_malicious.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
 
-# Script pentru verificarea fișierelor malitioase sau corupte
-
 file_path=$1
 
-# Verificăm dacă fișierul există
-if [ ! -f "$file_path" ]; then
-    echo "File $file_path not found!"
+if [ ! -r "$file_path" ] || [ ! -x "$file_path" ]; then
+    echo "Insufficient permissions to read or execute file: $file_path"
     exit 1
 fi
 
-# Verificăm numărul de linii
+echo "Checking file: $file_path"
+
 num_lines=$(wc -l < "$file_path")
 echo "Number of lines: $num_lines"
 
-# Verificăm numărul de cuvinte
 num_words=$(wc -w < "$file_path")
 echo "Number of words: $num_words"
 
-# Verificăm numărul de caractere
 num_chars=$(wc -c < "$file_path")
 echo "Number of characters: $num_chars"
 
-# Cautăm cuvinte cheie asociate cu fișiere corupte sau malitioase
-keywords=("corrupted" "dangerous" "risk" "attack" "malware" "malicious")
-
 echo "Checking for keywords:"
+keywords=("virus" "trojan" "malware" "spyware")
+
 for keyword in "${keywords[@]}"; do
-    grep -q "$keyword" "$file_path"
-    if [ $? -eq 0 ]; then
-        echo "File $file_path contains keyword '$keyword'"
-        exit 2
+    if grep -q "$keyword" "$file_path"; then
+        echo "Keyword \"$keyword\" found!"
     fi
 done
 
-# Verificăm existența de caractere non-ASCII
 echo "Checking for non-ASCII characters:"
-grep -q -P "[\x80-\xFF]" "$file_path"
-if [ $? -eq 0 ]; then
-    echo "File $file_path contains non-ASCII characters"
-    exit 3
+if grep -q -P '[^\x00-\x7F]' "$file_path"; then
+    echo "Non-ASCII characters found!"
 fi
 
-# Dacă nu s-a găsit nicio problemă, fișierul este considerat sigur
-echo "File $file_path is safe"
+safe_dir="DirectorSafe"
 
-exit 0
+if [ $num_lines -gt 100 ] || [ $num_words -gt 1000 ] || [ $num_chars -gt 10000 ]; then
+    echo "File $file_path is too large, moving to $safe_dir"
+    mv "$file_path" "$safe_dir/"
+    echo "File $file_path moved to $safe_dir"
+else
+    echo "File $file_path is safe"
+fi
